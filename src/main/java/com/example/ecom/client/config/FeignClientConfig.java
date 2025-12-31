58a5c91a3ca29a35a22e79756cff194a313928ce
Matthew Campbell <matt@moderne.io>
2025-12-31 17:27:57 -0500

    Java 17
    Co-authored-by: Moderne <team@moderne.io>

--- src/main/java/com/example/ecom/client/config/FeignClientConfig.java
+++ src/main/java/com/example/ecom/client/config/FeignClientConfig.java
@@ -45,8 +45,7 @@ public class FeignClientConfig {
 
                 // 1. If we are already authenticated with a JWT (incoming request), propagate
                 // it
-                if (authentication instanceof JwtAuthenticationToken) {
-                    JwtAuthenticationToken jwtToken = (JwtAuthenticationToken) authentication;
+                if (authentication instanceof JwtAuthenticationToken jwtToken) {
                     template.header("Authorization", "Bearer " + jwtToken.getToken().getTokenValue());
                 }
                 // 2. If not (e.g. background task or startup), use Client Credentials flow
